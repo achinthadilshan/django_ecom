e4ec9bd0f797134307d51ba5daa1d6d6524142ef
ACHii <>
2023-09-25 17:39:38 +0530

    update

--- store/admin.py
+++ store/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+
 admin.site.register(Customer)
-admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price']
+    search_fields = ['id', 'name', 'price']
+
+
+admin.site.register(Product, ProductAdmin)
+
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
--- store/models.py
+++ store/models.py
@@ -12,16 +12,25 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+    
 
 
 class Product(models.Model):
     name = models.CharField(max_length=64, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
-    # image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = 'https://fakeimg.pl/300x300?text=+'
+        return url
 
 
 class Order(models.Model):
@@ -33,6 +42,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 
 class OrderItem(models.Model):
@@ -42,6 +63,14 @@ class OrderItem(models.Model):
         Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
+    
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
@@ -56,4 +85,4 @@ class ShippingAddress(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.address
+        return self.city
