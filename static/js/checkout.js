248cf55eb4a0a19d38b8b2a4e809514d5bcbe592
ACHii <achii@Achinthas-MacBook-Air.local>
2023-09-27 15:42:25 +0530

    update

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -6,6 +6,37 @@ form.addEventListener('submit', (e) => {
   document.querySelector('#payment-info').classList.remove('d-none')
 })
 
+const submitFormData = () => {
+  console.log('Payment button clicked...')
+
+  const userFormData = {
+    name: null,
+    email: null,
+    total: total,
+  }
+
+  const shippingInfo = {
+    address: null,
+    city: null,
+    state: null,
+    zipcode: null,
+  }
+
+  if (!user) {
+    userFormData.name = form.name.value ? form.name.value : null
+    userFormData.email = form.email.value ? form.email.value : null
+  }
+
+  if (shipping) {
+    shippingInfo.address = form.address.value ? form.address.value : null
+    shippingInfo.city = form.city.value ? form.city.value : null
+    shippingInfo.state = form.state.value ? form.state.value : null
+    shippingInfo.zipcode = form.zipcode.value ? form.zipcode.value : null
+  }
+
+  processOrder(userFormData, shippingInfo)
+}
+
 document.querySelector('#make-payment').addEventListener('click', (e) => {
   submitFormData()
 })
@@ -28,6 +59,6 @@ const processOrder = async (userData, shippingData) => {
   const data = await res.json()
 
   console.log(data)
-
+  alert('Transaction completed!')
   window.location.href = '/'
 }
